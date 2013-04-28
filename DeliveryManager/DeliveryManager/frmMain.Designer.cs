cec42da40a3c57b821fcb9c65e7479fe33c5c602
Filipe Oliveira <contato@fmoliveira.com.br>
2013-04-28 00:51:25 -0300

    Criando aplicativo para receber os pedidos

---
+++ DeliveryManager/DeliveryManager/frmMain.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace DeliveryManager
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstPedidos = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DeliveryManager.Properties.Resources.mariopizzaiolo;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(99, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(673, 80);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pizza Delivery Manager";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lstPedidos
+            // 
+            this.lstPedidos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstPedidos.Location = new System.Drawing.Point(12, 99);
+            this.lstPedidos.Name = "lstPedidos";
+            this.lstPedidos.Size = new System.Drawing.Size(760, 450);
+            this.lstPedidos.TabIndex = 2;
+            this.lstPedidos.UseCompatibleStateImageBehavior = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.lstPedidos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Delivery Manager";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView lstPedidos;
+    }
+}
+
---
+++ DeliveryManager/DeliveryManager/frmMain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeliveryManager
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            prepararListaPedidos();
+        }
+
+        private void prepararListaPedidos()
+        {
+            lstPedidos.Items.Clear();
+            lstPedidos.Groups.Clear();
+            lstPedidos.Columns.Clear();
+
+            /* Ajustes gerais */
+            lstPedidos.View = View.Details;
+
+            /* Cria as colunas */
+            lstPedidos.Columns.Add("Data", 80);
+            lstPedidos.Columns.Add("Hora", 80);
+            lstPedidos.Columns.Add("Espera", 80);
+            lstPedidos.Columns.Add("Cliente", 150);
+            lstPedidos.Columns.Add("Bairro", 150);
+            lstPedidos.Columns.Add("Valor Total", 150);
+
+            /* Cria os grupos */
+            lstPedidos.Groups.Add("NovosPedidos", "Novos Pedidos");
+            lstPedidos.Groups.Add("EmProcessamento", "Em Processamento");
+            lstPedidos.Groups.Add("Finalizados", "Finalizados");
+        }
+    }
+}
